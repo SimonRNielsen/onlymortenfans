5164a0f4a5affdbbf98e31030fce99e6daec398a
SimonRNielsen <simonrn@gmail.com>
2026-03-12 10:26:19 +0100

    Testing working state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import ReactDOM from "react-dom/client";
+import { Test } from "./test.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <>
-        
+        <Test />
     </>
 );
