d2778cc8594dee16f6ebe2189e4a057e5b52a3aa
SimonRNielsen <simonrn@gmail.com>
2026-03-12 16:00:57 +0100

    Recieved verifyable response from API

--- src/login.js
+++ src/login.js
@@ -1,29 +1,65 @@
 import React from "react";
 import { useInput } from "./hooks";
 
+const loginURL = "https://reactapi-6jhi.onrender.com/api/users/login";
+
 export function LoginScreen(props) {
 
     let name = useInput("");
     let password = useInput("");
+    let inputDisabled = false;
 
-    function submitInfo(event) {
+    async function handleSubmit(event) {
+        
         event.preventDefault();
-        console.log(name);
-        console.log(password);
+
+        let loginDTO = {
+            email: name.value,
+            password: password.value
+        }
+
+        let response;
+
+        try {
+
+            response = await fetch(loginURL, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(loginDTO)
+            });
+
+        }
+        catch (error) {
+            console.error(error);
+        }
+
+        let responseData = await response.json();
+
+        console.log(responseData);
+
+        console.log("Input: " + name.value);
+        console.log("Input: " + password.value);
+
+    }
+
+    function disableInput() {
+        if (inputDisabled) return true;
+        return false;
+
     }
 
     return(
         <>
-        <form id="loginForm">
-            <label>Name:</label>
-            <input {...name}/>
+        <form id="loginForm" onSubmit={handleSubmit}>
+            <label>Email:</label>
+            <input {...name} />
             <br />
             <label>Password:</label>
             <input {...password}/>
             <br />
-            <button type="submit" onClick={submitInfo}>Login</button>
+            <button type="submit" disabled={disableInput()}>Login</button>
         </form>
-        <button>create</button>
+        <button disabled={disableInput()}>Create</button>
         </>
     );
 
