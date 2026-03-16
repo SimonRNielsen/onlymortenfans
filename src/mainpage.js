4a5cc36768a478fe6153a9153253e9232987c4d4
SimonRNielsen <simonrn@gmail.com>
2026-03-16 09:50:52 +0100

    merged

--- src/mainpage.js
+++ src/mainpage.js
@@ -14,8 +14,6 @@ export function MainPage() {
         <>
             { pageState === pageStates.NOT_LOGGED_IN ? <LoginScreen setPageState={setPageState} setUser={setUser}/> : <></> }
             { pageState === pageStates.LOGGED_IN && userInfo.user !== null ? <HolyWhiteboard setPageState={setPageState} setUser={setUser} userInfo={userInfo}/> : <></> }
-            { pageState === pageStates.CREATE_USER ? <h1>OPRET BRUGER</h1> : <></> }
-            { pageState === pageStates.LOGGED_IN && userInfo.user !== null ? <h1>HOLY BOARD</h1> : <></> }
             { pageState === pageStates.CREATE_USER ? <CreateScreen setPageState={setPageState} setUser={setUser}/> : <></> }
         </>
     );
